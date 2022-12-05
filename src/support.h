10fdcf4b0826de9a04d303a32e78999faf624358
Bo Svangård <bo.svangard@sylog.se>
2022-12-05 19:29:19 +0100

    wifi config

--- src/support.h
+++ src/support.h
@@ -5,9 +5,9 @@
 
 #define VERSION "2.6R5"
 
-##define WIFI_SSID "Test"
+//#define WIFI_SSID "Test"
 #define WIFI_SSID "SK0UX-Guest"
-##define WIFI_PASSWORD "qwertyqwerty"
+//#define WIFI_PASSWORD "qwertyqwerty"
 #define WIFI_PASSWORD "berget0065"
 #define HOSTNAME "MHI-AC-Ctrl2"
 
@@ -16,7 +16,8 @@
                                                     
 #define WiFI_SEARCH_FOR_STRONGER_AP_INTERVALL 12    // WiFi network re-scan interval in minutes with alternate to +5dB stronger signal if detected
 
-#define MQTT_SERVER "192.168.40.15"               // broker name or IP address of the broker
+//#define MQTT_SERVER "192.168.40.15"               // broker name or IP address of the broker
+#define MQTT_SERVER "10.1.2.170"               // broker name or IP address of the broker
 #define MQTT_PORT 1883                              // port number used by the broker
 #define MQTT_USER ""                                // if authentication is not used, leave it empty
 #define MQTT_PASSWORD ""                            // if authentication is not used, leave it empty
